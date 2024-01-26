be3044c9f1a0e3dc0e0cfc21bf939db1e9425b80
Paul McKenna <mckennapaul27@gmail.com>
2024-01-26 15:08:30 +0000

    commented out sign up page

--- app/(pages)/(auth)/sign-up/page.tsx
+++ app/(pages)/(auth)/sign-up/page.tsx
@@ -6,23 +6,23 @@ import { redirect, useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 // uncomment this to use this page and create an account
-export default function Page() {
-    const router = useRouter();
-    const { data: session, status: sessionStatus } = useSession();
-
-    useEffect(() => {
-        if (sessionStatus === 'authenticated') router.push('/dashboard');
-    }, [sessionStatus, router]);
-
-    if (sessionStatus === 'loading') return <FullPageLoader />;
-    if (sessionStatus !== 'authenticated') {
-        return <SignUpForm />;
-    }
-}
-
 // export default function Page() {
+//     const router = useRouter();
+//     const { data: session, status: sessionStatus } = useSession();
+
 //     useEffect(() => {
-//         redirect('/login');
-//     }, []);
-//     return <div></div>;
+//         if (sessionStatus === 'authenticated') router.push('/dashboard');
+//     }, [sessionStatus, router]);
+
+//     if (sessionStatus === 'loading') return <FullPageLoader />;
+//     if (sessionStatus !== 'authenticated') {
+//         return <SignUpForm />;
+//     }
 // }
+
+export default function Page() {
+    useEffect(() => {
+        redirect('/login');
+    }, []);
+    return <div>-</div>;
+}
